54e6b4a11ab489f519cd4f4eb4d67b5689b8e25d
Tony --Codex <dialatony34@gmail.com>
2025-10-02 11:50:11 +0100

    Tony

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,39 +1,47 @@
-const items = document.querySelectorAll(".carousel-item");
-const dots = document.querySelectorAll(".dot");
-const inner = document.querySelector(".carousel-inner");
+// const items = document.querySelectorAll(".carousel-item");
+// const dots = document.querySelectorAll(".dot");
+// const inner = document.querySelector(".carousel-inner");
 
-let currentIndex = 0;
-const total = items.length;
-const intervalTime = 4000; // 4 seconds
-let slideInterval;
+// let currentIndex = 0;
+// const total = items.length;
+// const intervalTime = 4000; // 4 seconds
+// let slideInterval;
 
-// Show a specific slide
-function showSlide(index) {
-  currentIndex = index;
-  inner.style.transform = `translateX(-${100 * index}%)`;;
-  dots.forEach((dot) => dot.classList.remove("active"));
-  dots[index].classList.add("active");
+// // Show a specific slide
+// function showSlide(index) {
+//   currentIndex = index;
+//   inner.style.transform = `translateX(-${100 * index}%)`;;
+//   dots.forEach((dot) => dot.classList.remove("active"));
+//   dots[index].classList.add("active");
+// }
+
+// // Next Slide
+// function nextSlide() {
+//   currentIndex = (currentIndex + 1) % total;
+//   showSlide(currentIndex);
+// }
+// // Auto Play
+// function startSlide() {
+//   slideInterval = setInterval(nextSlide, intervalTime);
+// }
+
+// // Manual Indicator Click
+// dots.forEach((dot, index) => {
+//   dot.addEventListener("click", () => {
+//     showSlide(index);
+//     clearInterval(slideInterval);
+//     startSlide();
+//   });
+// });
+
+// // Init
+// showSlide(0);
+// startSlide();
+
+
+
+const loader = () => {
+	const parentContainer = document.querySelector('.parent');
+	const loaderElement = document.getElementById('loader');
+	return { parentContainer, loaderElement };
 }
-
-// Next Slide
-function nextSlide() {
-  currentIndex = (currentIndex + 1) % total;
-  showSlide(currentIndex);
-}
-// Auto Play
-function startSlide() {
-  slideInterval = setInterval(nextSlide, intervalTime);
-}
-
-// Manual Indicator Click
-dots.forEach((dot, index) => {
-  dot.addEventListener("click", () => {
-    showSlide(index);
-    clearInterval(slideInterval);
-    startSlide();
-  });
-});
-
-// Init
-showSlide(0);
-startSlide();
